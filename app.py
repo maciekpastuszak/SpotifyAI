60e99674bc0aaad57ed5a3219e4f8b8b9b83dfa8
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-06-01 09:12:02 +0200

    add authentication

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 import spotipy
+from dotenv import dotenv_values
 
-spotipy.Spotify(
+config = dotenv_values(".env")
+
+
+sp = spotipy.Spotify(
     auth_manager=spotipy.SpotifyOAuth(
-        client_id=
-        client_secret=
-        redirect_uri="http://localhost:9999"
+        client_id=config["SPOTIFY_CLIENT_ID"],
+        client_secret=["SPOTIFY_CLIENT_SECRET"],
+        redirect_uri="http://localhost:9999",
     )
 )
+
+current_user = sp.current_user()
+print(current_user)
