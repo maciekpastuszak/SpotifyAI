40f73f9a7a6d1fb603a22790e6fa3095eb02fa8b
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-06-06 15:05:45 +0200

    add option to modify playlist

--- app.py
+++ app.py
@@ -9,10 +9,13 @@ sp = spotipy.Spotify(
         client_id=config["SPOTIFY_CLIENT_ID"],
         client_secret=["SPOTIFY_CLIENT_SECRET"],
         redirect_uri="http://localhost:9999",
+        scope="playlist-modify-private",
     )
 )
 
 current_user = sp.current_user()
 assert current_user is not None
 search_results = sp.search(q="Uptown Funk", type="track", limit=10)
-print(search_results["tracks"]["items"][0])
+print(search_results["tracks"]["items"][0]["id"])
+
+sp.user_playlist_create(current_user["id"], public=False, name="TESTIN PLAYLIST")
