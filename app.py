621f09a2667fba5ef35cbd13b68c8e3059470f33
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-06-07 10:52:05 +0200

    retrive adding playlist to the account

--- app.py
+++ app.py
@@ -74,8 +74,8 @@ for item in playlist:
     search_results = sp.search(q=query, type="track", limit=10)
     track_ids.append(search_results["tracks"]["items"][0]["id"])
 
-# created_playlist = sp.user_playlist_create(
-#     current_user["id"], public=False, name="TESTIN PLAYLIST"
-# )
+created_playlist = sp.user_playlist_create(
+    current_user["id"], public=False, name="TESTIN PLAYLIST"
+)
 
-# sp.user_playlist_add_tracks(current_user["id"], created_playlist["id"], tracks)
+sp.user_playlist_add_tracks(current_user["id"], created_playlist["id"], track_ids)
